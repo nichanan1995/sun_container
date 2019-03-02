e5e297864d300d69cbe327ab0cae5f89d37fcd80
nichanan <nichanan.j@sunteen.co.th>
2019-03-02 15:17:51 +0700

    chang bg ok

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,25 +6,30 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Sun Container',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sun Container'),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-              border: Border.all(
-                  width: 5.0, color: Colors.green, style: BorderStyle.solid),
-              borderRadius: BorderRadius.circular(20.0),
-              gradient: LinearGradient(
-                  colors: [Colors.white, Colors.orange],
-                  begin: Alignment.topCenter),
-              color: Colors.yellow[200]),
-          constraints: BoxConstraints.expand(width: 200.0),
-          padding: EdgeInsets.all(50.0),
-          margin: EdgeInsets.all(40.0),
-          alignment: Alignment.topCenter,
-          child: Text('Sun Container'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Sun Container'),
+          ),
+          body: Container(
+            color: Colors.black,
+            alignment: Alignment(0, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      width: 5.0,
+                      color: Colors.green,
+                      style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(20.0),
+                  gradient: LinearGradient(
+                      colors: [Colors.white, Colors.orange],
+                      begin: Alignment.topCenter),
+                  color: Colors.yellow[200]),
+              constraints: BoxConstraints.expand(width: 200.0),
+              padding: EdgeInsets.all(50.0),
+              margin: EdgeInsets.all(40.0),
+              alignment: Alignment.topCenter,
+              child: Text('Sun Container'),
+            ),
+          )),
     );
   }
 }
